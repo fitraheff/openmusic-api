e6c787410baa4986b740d8359c5ae457dd34a3d8
fitraheff <mhmdfitrah24@gmail.com>
2025-01-22 22:10:17 +0700

    buat penanganan error, validator. tinggal lanjut services

---
+++ src/services/AlbumsService.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class AlbumsService {
+    constructor() {
+        this._pool = new Pool();
+    }
+
+    
+    }
